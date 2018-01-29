e30bdd20c16bb83936b843916b023088e1b0d1d7
Clément Lemoine <clementlemoine.cl@gmail.com>
2018-01-29 18:03:52 +0100

    :art: responsive refs anim

--- src/js/animRefs.js
+++ src/js/animRefs.js
@@ -9,10 +9,7 @@ module.exports = function(refs){
         loop = !loop;
         references.each(function(i){
             const coeff = (i + loop) % 2 ? -1 : 1;
-            TweenLite.to($(this), 0.6, {
-                y: coeff * 10 + (Math.random() * 5 - 5),
-                ease: Power2.easeInOut
-            });
+            TweenLite.to($(this), 0.6, { y: coeff * 10 + (Math.random() * 5 - 5), ease: Power2.easeInOut});
         });
         if(windowWidth > 960) TweenLite.delayedCall(13, run, [references]);
     }
